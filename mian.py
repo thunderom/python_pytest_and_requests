f12c6b5c515642eb7aaa0d0b957476340531fa67
Roman Grand <grand@internet.ru>
2024-08-16 05:12:30 +0700

    Add files via upload

---
+++ mian.py
@@ -0,0 +1,32 @@
+import requests
+
+URL = 'https://api.pokemonbattle.ru/v2'
+TOKEN = 'fc8c104344becb877765a9f4792d1280'
+HEADER = {
+    'Content-Type': 'application/JSON',
+    'trainer_token': TOKEN
+    }
+body_create_pokemon = {
+    "name": "generate",
+    "photo_id": -1
+    }
+
+response_create_pokemon = requests.post(url = f'{URL}/pokemons', headers = HEADER, json = body_create_pokemon)
+pokemon_id = response_create_pokemon.json()['id']
+print(response_create_pokemon.text)
+
+body_change_name_pokemon = {
+    "pokemon_id": pokemon_id,
+    "name": "Dudochnik"
+    }
+
+response_change_name_pokemon = requests.patch(url = f'{URL}/pokemons', headers = HEADER, json = body_change_name_pokemon)
+print(response_change_name_pokemon.text)
+
+
+body_add_pokemon_to_pokeball = {
+    "pokemon_id": pokemon_id
+    }
+
+response_add_pokemon_to_pokeball = requests.post(url = f'{URL}/trainers/add_pokeball', headers = HEADER, json = body_add_pokemon_to_pokeball)
+print(response_add_pokemon_to_pokeball.text)
---
+++ tests/test_pokemon.py
@@ -0,0 +1,42 @@
+import requests
+import pytest
+
+URL = 'https://api.pokemonbattle.ru/v2'
+TOKEN = 'your_token_must_be_here'
+HEADER = {
+    'Content-Type': 'application/JSON',
+    'trainer_token': TOKEN
+    }
+TRAINER_ID = '4487'
+
+def test_get_trainers():
+    response_get_trainers = requests.get(
+        url = f'{URL}/trainers',
+        headers = HEADER
+        )
+    assert response_get_trainers.status_code == 200
+
+def test_get_my_trainer_name():
+    response_get_my_trainer_name = requests.get(
+        url = f'{URL}/trainers',
+        headers = HEADER,
+        params = {'trainer_id': TRAINER_ID}
+        )
+    assert response_get_my_trainer_name.json()['data'][0]['trainer_name'] == 'минайчи'
+
+@pytest.mark.parametrize('key, value', [
+    ('id', TRAINER_ID),
+    ('trainer_name', 'минайчи'),
+    ('level', '1'),
+    ('get_history_battle', '0'),
+    ('is_premium', False),
+    ('premium_duration', 0),
+    ('city', 'новосибирск')
+    ])
+def test_parametrize(key, value):
+    response_get_my_trainer = requests.get(
+        url = f'{URL}/trainers',
+        headers = HEADER,
+        params = {'trainer_id': TRAINER_ID}
+        )
+    assert response_get_my_trainer.json()['data'][0][key] == value

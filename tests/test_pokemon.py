62fb82e7d17cd443e3b1a690e92004ca0584430f
thunderom <grand@internet.ru>
2024-08-19 18:11:45 +0700

    changed consts

--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -3,9 +3,10 @@ import pytest
 from dotmap import DotMap
 
 URL = "https://api.pokemonbattle.ru/v2"
-TOKEN = "your_token_must_be_here"
+TOKEN = "your_token_must_be_here" #PASTE YOUR TOKEN HERE
 HEADER = {"Content-Type": "application/JSON", "trainer_token": TOKEN}
-TRAINER_ID = "4487"
+TRAINER_ID = "your_trainer_id_must_be_here" #PASTE YOUR TRAINER_ID HERE
+TRAINER_NAME = "your_trainer_name_must_be_here" #PASTE YOUR TRAINER_NAME HERE
 
 def test_get_trainers():
     response_get_trainers = requests.get(url=f"{URL}/trainers", headers=HEADER)
@@ -16,13 +17,13 @@ def test_get_my_trainer_name():
         url=f"{URL}/trainers", headers=HEADER, params={"trainer_id": TRAINER_ID}
     )
     object = DotMap(response_get_my_trainer_name.json())
-    assert object.data[0].trainer_name == "минайчи"
+    assert object.data[0].trainer_name == TRAINER_NAME
 
 @pytest.mark.parametrize(
     "key, value",
     [
         ("id", TRAINER_ID),
-        ("trainer_name", "минайчи"),
+        ("trainer_name", TRAINER_NAME),
         ("level", "1"),
         ("get_history_battle", "0"),
         ("is_premium", False),
@@ -30,7 +31,6 @@ def test_get_my_trainer_name():
         ("city", "новосибирск"),
     ],
 )
-
 def test_parametrize(key, value):
     response_get_my_trainer = requests.get(
         url=f"{URL}/trainers", headers=HEADER, params={"trainer_id": TRAINER_ID}
